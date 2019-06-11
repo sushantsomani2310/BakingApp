d2e8913c50cb5fa2518e78148d15b28882aad633
sushantsomani2310 <34996959+sushantsomani2310@users.noreply.github.com>
2019-06-11 17:31:30 +0530

    Update RecipeDetailActivity.java

--- app/src/main/java/com/example/bakingapp/views/RecipeDetailActivity.java
+++ app/src/main/java/com/example/bakingapp/views/RecipeDetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.bakingapp.views;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 
 import com.example.bakingapp.R;
 import com.example.bakingapp.datasource.RecipeStepsAdapter;
+import com.example.bakingapp.views.fragments.RecipeStepsFragment;
 import com.example.bakingapp.views.fragments.RecipeVideoStepFragment;
 
 public class RecipeDetailActivity extends AppCompatActivity {
@@ -16,25 +18,32 @@ public class RecipeDetailActivity extends AppCompatActivity {
     private TextView ingredientsTextView;
     private RecipeStepsAdapter recipeStepsAdapter;
     private boolean isMultiPane = false;
+    private FragmentManager fragmentManager;
+    private RecipeStepsFragment stepFrag;
 
     @Override
     protected void onCreate(Bundle onSavedInstanceState){
         super.onCreate(onSavedInstanceState);
         setContentView(R.layout.recipe_detail_layout);
 
+        fragmentManager = getSupportFragmentManager();
+        stepFrag = (RecipeStepsFragment) fragmentManager.findFragmentById(R.id.recipe_detail_steps_fragment);
+
         if(findViewById(R.id.recipe_video_tablet_pane)!=null){
             // make tablet layout
             isMultiPane = true;
             createTabletLayout(onSavedInstanceState);
         }
+        else stepFrag.setIsMultiPane(false);
     }
 
     private void createTabletLayout(Bundle onSavedInstanceState){
         RecipeVideoStepFragment videoStepFragment = new RecipeVideoStepFragment();
-        Bundle fragmentBundle = new Bundle();
-        fragmentBundle.putBoolean("isMultiPane",isMultiPane);
-        videoStepFragment.setArguments(fragmentBundle);
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        //Bundle fragmentBundle = new Bundle();
+        //fragmentBundle.putBoolean("isMultiPane",isMultiPane);
+        //videoStepFragment.setArguments(fragmentBundle);
+
+        stepFrag.setIsMultiPane(true);
 
         fragmentManager.beginTransaction()
                 .add(R.id.video_container,videoStepFragment)
