0cb522146cffbd6cc96dd7179aa1f07aceab8558
sushantsomani2310 <34996959+sushantsomani2310@users.noreply.github.com>
2019-06-19 17:28:22 +0530

    Update RecipeVideoActivity.java

--- app/src/main/java/com/example/bakingapp/views/RecipeVideoActivity.java
+++ app/src/main/java/com/example/bakingapp/views/RecipeVideoActivity.java
@@ -21,6 +21,7 @@ public class RecipeVideoActivity extends AppCompatActivity {
     private List<RecipeSteps> recipeSteps;
     private ImageView prevButton,nextButton;
     private FragmentManager fragmentManager;
+    private boolean isLandscape=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +29,7 @@ public class RecipeVideoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recipe_video);
 
         prevButton = findViewById(R.id.prev_button);
+        if(prevButton==null) isLandscape = true;
         nextButton = findViewById(R.id.next_button);
         recipeSteps = (ArrayList<RecipeSteps>) getIntent().getSerializableExtra("RECIPE_STEPS_LIST");
         stepIndex = (int) getIntent().getSerializableExtra("RECIPE_STEP_INDEX");
@@ -39,76 +41,77 @@ public class RecipeVideoActivity extends AppCompatActivity {
                 .add(R.id.video_container,videoStepFragment)
                 .commit();
 
-        Bundle descBundle = new Bundle();
-        descBundle.putString("StepDesc",step.getDescription());
-        RecipeDescriptionFragment descriptionFragment = new RecipeDescriptionFragment();
-        descriptionFragment.setArguments(descBundle);
-        fragmentManager.beginTransaction()
-                .add(R.id.description_container,descriptionFragment)
-                .commit();
+        if(!isLandscape) {
+            Bundle descBundle = new Bundle();
+            descBundle.putString("StepDesc", step.getDescription());
+            RecipeDescriptionFragment descriptionFragment = new RecipeDescriptionFragment();
+            descriptionFragment.setArguments(descBundle);
+            fragmentManager.beginTransaction()
+                    .add(R.id.description_container, descriptionFragment)
+                    .commit();
+        }
     }
 
     @Override
     public void onStart(){
         super.onStart();
-        //recipeSteps,recipeStepIndex
-        prevButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(stepIndex!=0){
-                    // play previous video and show previous description
-                    stepIndex--;
-                    RecipeSteps futureStep = recipeSteps.get(stepIndex);
-                    Bundle descBundle = new Bundle();
-                    descBundle.putString("StepDesc",futureStep.getDescription());
-                    RecipeDescriptionFragment descriptionFragment = new RecipeDescriptionFragment();
-                    descriptionFragment.setArguments(descBundle);
-                    fragmentManager.beginTransaction()
-                            .replace(R.id.description_container,descriptionFragment)
-                            .commit();
+        if(!isLandscape) {
+            prevButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (stepIndex != 0) {
+                        // play previous video and show previous description
+                        stepIndex--;
+                        RecipeSteps futureStep = recipeSteps.get(stepIndex);
+                        Bundle descBundle = new Bundle();
+                        descBundle.putString("StepDesc", futureStep.getDescription());
+                        RecipeDescriptionFragment descriptionFragment = new RecipeDescriptionFragment();
+                        descriptionFragment.setArguments(descBundle);
+                        fragmentManager.beginTransaction()
+                                .replace(R.id.description_container, descriptionFragment)
+                                .commit();
 
-                    RecipeVideoStepFragment recipeVideoStepFragment = new RecipeVideoStepFragment();
-                    Bundle videoBundle = new Bundle();
-                    videoBundle.putSerializable("RecipeStep",futureStep);
-                    recipeVideoStepFragment.setArguments(videoBundle);
-                    fragmentManager.beginTransaction()
-                            .replace(R.id.video_container,recipeVideoStepFragment)
-                            .commit();
+                        RecipeVideoStepFragment recipeVideoStepFragment = new RecipeVideoStepFragment();
+                        Bundle videoBundle = new Bundle();
+                        videoBundle.putSerializable("RecipeStep", futureStep);
+                        recipeVideoStepFragment.setArguments(videoBundle);
+                        fragmentManager.beginTransaction()
+                                .replace(R.id.video_container, recipeVideoStepFragment)
+                                .commit();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "This is first step", Toast.LENGTH_LONG).show();
+                    }
                 }
-                else{
-                    Toast.makeText(getApplicationContext(),"This is first step",Toast.LENGTH_LONG).show();
-                }
-            }
-        });
+            });
 
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int totalSteps = recipeSteps.size();
-                if(stepIndex!=(totalSteps-1)){
-                    // play next video and show next description
-                    stepIndex++;
-                    RecipeSteps futureStep = recipeSteps.get(stepIndex);
-                    Bundle descBundle = new Bundle();
-                    descBundle.putString("StepDesc",futureStep.getDescription());
-                    RecipeDescriptionFragment descriptionFragment = new RecipeDescriptionFragment();
-                    descriptionFragment.setArguments(descBundle);
-                    fragmentManager.beginTransaction()
-                            .replace(R.id.description_container,descriptionFragment)
-                            .commit();
+            nextButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int totalSteps = recipeSteps.size();
+                    if (stepIndex != (totalSteps - 1)) {
+                        // play next video and show next description
+                        stepIndex++;
+                        RecipeSteps futureStep = recipeSteps.get(stepIndex);
+                        Bundle descBundle = new Bundle();
+                        descBundle.putString("StepDesc", futureStep.getDescription());
+                        RecipeDescriptionFragment descriptionFragment = new RecipeDescriptionFragment();
+                        descriptionFragment.setArguments(descBundle);
+                        fragmentManager.beginTransaction()
+                                .replace(R.id.description_container, descriptionFragment)
+                                .commit();
 
-                    RecipeVideoStepFragment recipeVideoStepFragment = new RecipeVideoStepFragment();
-                    Bundle videoBundle = new Bundle();
-                    videoBundle.putSerializable("RecipeStep",futureStep);
-                    recipeVideoStepFragment.setArguments(videoBundle);
-                    fragmentManager.beginTransaction()
-                            .replace(R.id.video_container,recipeVideoStepFragment)
-                            .commit();
+                        RecipeVideoStepFragment recipeVideoStepFragment = new RecipeVideoStepFragment();
+                        Bundle videoBundle = new Bundle();
+                        videoBundle.putSerializable("RecipeStep", futureStep);
+                        recipeVideoStepFragment.setArguments(videoBundle);
+                        fragmentManager.beginTransaction()
+                                .replace(R.id.video_container, recipeVideoStepFragment)
+                                .commit();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "No more steps", Toast.LENGTH_LONG).show();
+                    }
                 }
-                else{
-                    Toast.makeText(getApplicationContext(),"No more steps",Toast.LENGTH_LONG).show();
-                }
-            }
-        });
+            });
+        }
     }
 }
