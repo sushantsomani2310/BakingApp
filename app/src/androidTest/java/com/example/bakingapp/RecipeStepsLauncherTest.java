2ade903df928a469b1885641763f448d5e4c048d
sushantsomani2310 <34996959+sushantsomani2310@users.noreply.github.com>
2019-06-25 14:52:15 +0530

    Update RecipeStepsLauncherTest.java

--- app/src/androidTest/java/com/example/bakingapp/RecipeStepsLauncherTest.java
+++ app/src/androidTest/java/com/example/bakingapp/RecipeStepsLauncherTest.java
@@ -1,7 +1,18 @@
 package com.example.bakingapp;
 
-import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -11,6 +22,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+
 @RunWith(AndroidJUnit4.class)
 public class RecipeStepsLauncherTest {
 
@@ -19,7 +33,16 @@ public class RecipeStepsLauncherTest {
 
     @Test
     public void tapRecipeItem(){
-        int sfd=45;
+        try {
+            //onView(ViewMatchers.withId(R.id.recipe_recycler_view))
+              //      .perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
+            ViewInteraction vi = onView(ViewMatchers.withId(R.id.recipe_recycler_view));
+            vi.perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
 
+            int sdf = 45;
+        }
+        catch (Exception ex){
+            int sfsd= 34;
+        }
     }
 }
