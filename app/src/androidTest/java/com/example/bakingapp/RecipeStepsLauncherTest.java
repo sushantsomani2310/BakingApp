4ac8b6052ee3d471e8dbdcd5f1d3e9339b5827a0
sushantsomani2310 <34996959+sushantsomani2310@users.noreply.github.com>
2019-06-29 19:21:54 +0530

    Update RecipeStepsLauncherTest.java

--- app/src/androidTest/java/com/example/bakingapp/RecipeStepsLauncherTest.java
+++ app/src/androidTest/java/com/example/bakingapp/RecipeStepsLauncherTest.java
@@ -2,17 +2,24 @@ package com.example.bakingapp;
 
 import android.support.v7.widget.RecyclerView;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.anything;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.hasToString;
+import static org.hamcrest.Matchers.startsWith;
+import static org.hamcrest.core.AllOf.allOf;
 import static org.junit.Assert.assertEquals;
 
 import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.ViewAction.*;
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -34,12 +41,10 @@ public class RecipeStepsLauncherTest {
     @Test
     public void tapRecipeItem(){
         try {
-            //onView(ViewMatchers.withId(R.id.recipe_recycler_view))
-              //      .perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
-            ViewInteraction vi = onView(ViewMatchers.withId(R.id.recipe_recycler_view));
-            vi.perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
-
+            //onView(allOf(withId(R.id.recipe_name_textview), withText("Yellow Cake"))).check(matches(isDisplayed()));
+            onData(allOf(withId(R.id.recipe_name_textview), withText("Brownies"))).check(matches(isDisplayed())).perform(click());
             int sdf = 45;
+            // onData(anything()).inAdapterView(withId(R.id.tea_grid_view)).atPosition(1).perform(click());
         }
         catch (Exception ex){
             int sfsd= 34;
